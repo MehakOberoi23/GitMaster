fff130c1573cb427a524cf1b622b99d2a85e5e40
Mehak Oberoi <Mehak Oberoi@DESKTOP-V43PJ6C>
2023-06-27 15:57:37 +0530

    new code

---
+++ GitDemoProject/src/GitLearning/FirstGit.java
@@ -0,0 +1,11 @@
+package GitLearning;
+
+public class FirstGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, World!");
+	}
+
+}
